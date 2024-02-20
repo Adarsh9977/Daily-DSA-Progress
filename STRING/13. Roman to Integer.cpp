018d88511336028fffd676c220b3fd08dd956bd8
Adarsh Tiwari <adarshtiwari797023@gmail.com>
2024-02-20 23:56:57 +0530

    13. Roman to Integer

---
+++ STRING/13. Roman to Integer.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+        int check(char a){
+    int res = 0;
+    switch (a)
+    {
+    case 'I':
+    {
+        res = 1;
+        break;
+    }
+    case 'V':
+    {
+        res = 5;
+        break;
+    }
+    case 'X':
+    {
+        res = 10;
+        break;
+    }
+    case 'L':
+    {
+        res = 50;
+        break;
+    }
+    case 'C':
+    {
+        res = 100;
+        break;
+    }
+    case 'D':
+    {
+        res = 500;
+        break;
+    }
+    case 'M':
+    {
+        res = 1000;
+        break;
+    }
+    default: break;
+    }
+    return res;
+}
+
+    int romanToInt(string s) {
+         int n=s.size();
+    int ans=0;
+
+    for(int i=0;i<n;i++){
+        if(check(s[i])>=check(s[i+1])){
+            ans+=(check(s[i]));
+        }
+        else{
+            ans-=(check(s[i]));
+        }
+    }
+    return ans;
+    }
+};
