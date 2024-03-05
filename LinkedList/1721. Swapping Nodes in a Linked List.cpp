dd03bb757ece8e393f505d07db42398c5864be38
Adarsh Tiwari <adarshtiwari797023@gmail.com>
2024-03-05 08:25:07 +0530

    1721. Swapping Nodes in a Linked List

--- LinkedList/1721. Swapping Nodes in a Linked List.cpp
+++ LinkedList/1721. Swapping Nodes in a Linked List.cpp
@@ -18,3 +18,43 @@ public:
         return ans;
     }
 };
+
+
+
+
+//Approach-2 without using array
+
+class Solution {
+public:
+    int Length(ListNode* head){
+        ListNode* temp=head;
+        int cnt=0;
+        while(temp!=NULL){
+            temp=temp->next;
+            cnt++;
+        }
+        return cnt;
+    }
+    ListNode* find(ListNode* head, int k){
+        while(k>1){
+            head=head->next;
+            k--;
+        }
+        return head;
+    }
+
+    ListNode* swapNodes(ListNode* head, int k) {
+        int n=Length(head);
+
+        ListNode* node1=find(head, k);
+        
+
+        ListNode* node2=find(head, n-k+1);
+        
+
+        swap(node1->val, node2->val);
+        return head;
+
+        
+    }
+};
